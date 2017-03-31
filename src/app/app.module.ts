7c9d0936321a7d6ed5b2a9e94f78a3ad5f631526
Raziyeh Bazargan <raziyeh.bazargan@eventcore.com>
2017-03-31 13:24:28 -0700

    complete binding , master/detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms'; // for using NgModel and input tg
-
+import {HeroDetailComponent} from "./hero-detail.component";
 import { AppComponent }  from './app.component';
 
 @NgModule({
@@ -9,7 +9,13 @@ import { AppComponent }  from './app.component';
     BrowserModule,
     FormsModule  // <-- import the FormsModule before binding with [(ngModel)]
   ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    /*the declarations array contains a list of application
+     components, pipes, and directives that belong to the
+     module.A component must be declared in a module before other components can reference it.*/
+    AppComponent,
+    HeroDetailComponent
+  ],
+  bootstrap:  [ AppComponent ]
 })
 export class AppModule { }
